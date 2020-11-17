3b18df7dae08f5c363e3d4d7c302bc96e7031548
Artyom Kozlov <a.kozlov@roowix.com>
2020-11-17 20:22:22 +0700

    init

---
+++ precache-manifest.aa017ae0af6aef1ad17d71ec37e122df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c29fb0d647710eae91",
+    "url": "/css/app.100dc2d4.css"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/fonts/gotham-pro.3100f91b.ttf"
+  },
+  {
+    "revision": "ca792a7a940dc59ae24355cccd869402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46c29fb0d647710eae91",
+    "url": "/js/app.0df71aab.js"
+  },
+  {
+    "revision": "c4780c95dd3ce7c5f16a",
+    "url": "/js/chunk-vendors.8125377d.js"
+  },
+  {
+    "revision": "9f7b0642781549922d4390397a32cc98",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
